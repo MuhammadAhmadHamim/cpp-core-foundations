486f08f7adc792f72f3cd1f6d10e3593f7e90a57
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-04 21:50:57 +0500

    Printed a Diamond shape in c++..

---
+++ 01_Fundamentals/01.2_Diamond.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"Let's craft a Diamond..\n"<<endl;
+	cout<<"\t \t * \t \t\n";
+	cout<<"\t *";
+	cout<<"\t *";
+	cout<<"\t *\n";
+	cout<<" * \t *";
+	cout<<"\t * \t *";
+	cout<<"\t *\n";
+	cout<<"\t *";
+	cout<<"\t *";
+	cout<<"\t *\n";
+	cout<<"\t \t * \t \t\n";
+	getch();
+	return 0;
+}
