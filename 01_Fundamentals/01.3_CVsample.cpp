64afe6be4cb334b0396a7a9a59279067e4edb2d2
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-04 21:56:26 +0500

    Printed a template CV model!

---
+++ 01_Fundamentals/01.3_CVsample.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"Name: ____________"<<"\n";
+	cout<<"Father Name: _________"<<"\n";
+	cout<<"CNIC#:- _____-_____-_"<<"\n";
+	cout<<"Qualification: ______"<<"\n";
+	cout<<"Semester:___"<<"\n";
+	cout<<"CGPA: ____"<<"\n";
+		getch();
+	return 0;}
