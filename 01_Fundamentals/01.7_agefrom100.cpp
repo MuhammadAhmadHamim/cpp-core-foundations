05aa9d75b696fa45649ee710930f24d13c015d0b
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-06 16:52:10 +0500

    Let's check how much more years you have before 100..

---
+++ 01_Fundamentals/01.7_agefrom100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write your code below
+    int age;
+    int agefrom100;
+    cout<<"Enter your age"<<endl;
+cin>>age;
+agefrom100=100-age;
+cout<<agefrom100<<" years till 100"<<endl;
+
+    return 0;
+}
