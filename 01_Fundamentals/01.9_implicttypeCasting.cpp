4121d2ed1ecbe09e172dd4b2f22c1daabae84980
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-07 20:24:39 +0500

    Implicitly casting the type..

---
+++ 01_Fundamentals/01.9_implicttypeCasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main() 
+{
+//Implict Type Casting
+int a=5;
+double b=2.5456;
+double sum=a+b;
+cout<<"Sum:"<<sum<<endl;
+	getch(); 
+    return 0;
+}
