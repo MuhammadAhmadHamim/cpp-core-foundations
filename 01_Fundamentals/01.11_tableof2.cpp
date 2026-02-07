1d81a4077f67d2d78746a1edacffbd9a071950c6
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-07 20:27:28 +0500

    Writing the table of 2..

---
+++ 01_Fundamentals/01.11_tableof2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a=2;
+	cout<<"2x1= "<<a*1<<endl;
+		cout<<"2x2= "<<a*2<<endl;
+			cout<<"2x3= "<<a*3<<endl;
+	cout<<"2x4= "<<a*4<<endl;
+	cout<<"2x5= "<<a*5<<endl;
+	cout<<"2x6= "<<a*6<<endl;
+	cout<<"2x7= "<<a*7<<endl;
+	cout<<"2x8= "<<a*8<<endl;
+	cout<<"2x9= "<<a*9<<endl;
+	cout<<"2x10= "<<a*10<<endl;
+		getch();
+return 0;
+
+	
+	
+	
+}
