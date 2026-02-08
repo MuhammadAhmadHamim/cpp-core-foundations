4ee0942ce519d462113a1fdcf3982ede3f2cd332
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-08 18:50:06 +0500

    Writing the table of 2 using loop..

---
+++ 01_Fundamentals/01.14_tableof2withForLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int i;
+	for(i= 2;i <=20;i+=2){
+		cout<<"2 x "<<(i/2)<<" = "<<i<<endl;
+	} 
+		getch();
+	return 0;
+	
+	
+}
