36ba3bd7bceb710da9fb4f4afd8d1a3479eaeb07
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-10 21:14:33 +0500

    Sum till a specific number...

---
+++ 01_Fundamentals/01.18_sumtillN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+int N;
+int sum = 0;
+cout<<"Enter the value till whom you wanna print the sum of even numbers from 1:";
+cin>>N;
+for(int i=1 ; i<=N; i+=1)
+{
+	
+	cout<<"Adding "<<i<<" to the current sum which is "<<sum<<" it becomes "<<sum+i<<endl;
+	sum+=i;
+}
+cout<<"The final sum of even numbers from 1 to "<<N<<" is "<<sum<<endl;
+	getch();
+return 0;
+}
