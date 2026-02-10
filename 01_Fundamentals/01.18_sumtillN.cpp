5e57eba3d9543d6947a1dc570b93bed2cf8b0c63
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-10 21:24:50 +0500

    Sum till a specific number...

--- 01_Fundamentals/01.18_sumtillN.cpp
+++ 01_Fundamentals/01.18_sumtillN.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 int N;
 int sum = 0;
-cout<<"Enter the value till whom you wanna print the sum of even numbers from 1:";
+cout<<"Enter the value till whom you wanna print the sum of numbers from 1:";
 cin>>N;
 for(int i=1 ; i<=N; i+=1)
 {
