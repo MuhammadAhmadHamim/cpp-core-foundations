95a71b7139aeeb63b9ed561bd0cd204b6cbcdd99
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-17 20:51:57 +0500

    The basic math operations performed on two numbers..

---
+++ 01_Fundamentals/01.35_twonumberoperations.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a=12;
+	int b=6;
+	
+	cout<<"Let us take two numbers.."<<endl;
+	cout<<"The first one is "<<a<<endl;
+		cout<<"The second one is "<<b<<endl;
+		cout<<"\n";
+		cout<<"Sum:- 12+6="<<a+b<<endl;
+		cout<<"Difference:- 12-6="<<a-b<<endl;
+		cout<<"Product:- 12x6="<<a*b<<endl;
+		cout<<"Quotient:- 12/6="<<a/b<<endl;
+		cout<<"Remainder:- "<<a%b<<endl;
+		return 0;
+		
+		
+		
+
+	
+	
+	
+}
