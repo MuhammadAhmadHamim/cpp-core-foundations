17860b87a2fc31bfd97ea52825b66543d4587155
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-17 20:53:04 +0500

    Trying to swap the values of two datatypes without using swap()

---
+++ 01_Fundamentals/01.36_manualswap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	float a=10;
+	float b=20;
+	cout<<"Before swapping.."<<endl;
+	cout<<"a= "<<a<<endl;
+	cout<<"b= "<<b<<endl;
+	float temp=a;
+	a=b;
+	b=temp;
+	cout<<"After swapping.."<<endl;
+	cout<<"a= "<<a<<endl;
+		cout<<"b= "<<b<<endl;
+
+	return 0;
+}
