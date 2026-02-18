4be13d6246212487bf174d6afb77cc6dec8768b0
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-02-18 20:30:42 +0500

    Printing N through for-loop

---
+++ 01_Fundamentals/01.39_naturalNumbersinForloop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int a;
+		 cout<<"Let's print the Numbers from 1 to 10 using For Loop.."<<endl;
+		       for(a=1;+a<=10;a++){
+		       	      cout<<a;
+		       	    if(a<10){
+		       	    	         cout<<', '<<endl;
+					   }
+			   }
+
+
+
+
+
+
+
+
+
+	return 0;
+}
