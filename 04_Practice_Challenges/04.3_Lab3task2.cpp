8476f874efbb5f9be7b0268fc15c89f38a7bf41e
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-10 23:41:59 +0500

    Lab 3 task 2..

---
+++ 04_Practice_Challenges/04.3_Lab3task2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"Let's perform some basic expressions according to C++ language rules..\n"<<endl;
+	cout<<"Expressions \t \t \t \t \t";
+	cout<<"Value"<<endl;
+	cout<<"28/4-2 \t \t \t \t \t \t  ";
+	cout<<28/4-2<<endl;
+	cout<<"6+12*2-8 \t \t \t \t \t ";
+	cout<<6+12*2-8<<endl;
+	cout<<"4+8*2 \t \t \t \t \t \t ";
+	cout<<4+8*2<<endl;
+	cout<<"6+17%3-2 \t \t \t \t \t  ";
+	cout<<6+17%3-2<<endl;
+	cout<<"6+17%3-2 \t \t \t \t \t  ";
+	cout<<6+17%3-2<<endl;
+	cout<<"2+22*(9-7) \t \t \t \t \t ";
+	cout<<2+22*(9-7)<<endl;
+	cout<<"(8+7)*2 \t \t \t \t \t ";
+	cout<<(8+7)*2<<endl;
+	cout<<"(16+7)%2-1 \t \t \t \t \t  ";
+	cout<<(16+7)%2-1<<endl;
+	cout<<"12/(10-6) \t \t \t \t \t  ";
+	cout<<12/(10-6)<<endl;
+	cout<<"(19-3)*(2+2)/4 \t \t \t \t \t ";
+	cout<<(19-3)*(2+2)/4 <<endl;
+		getch();
+	return 0;	
+}
