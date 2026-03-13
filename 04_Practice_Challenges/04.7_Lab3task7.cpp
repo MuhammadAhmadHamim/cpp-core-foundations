ac3457f8a16a9caf6d77aedd4ec2d4d24c0333dd
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-13 11:26:39 +0500

    Lab 3 Task 7

---
+++ 04_Practice_Challenges/04.7_Lab3task7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a=28;
+	int b=32;
+	int c=37;
+	int d=24;
+	int e=33;
+	float sum;
+	
+	sum=a+b+c+d+e;
+	cout<<"The sum of the given numbers is "<<sum<<endl;
+	cout<<"The number of values is 5"<<endl;
+	sum=float(sum);
+	cout<<"The average of the given numbers is "<<sum/5<<endl;
+		getch();
+	return 0;
+
+	
+}
