0c7f862c7a33b624bc51389bb30fb8efc4564249
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-15 01:30:59 +0500

    Lab 3 Task Part1

--- 04_Problem_Solving/04.8_Lab3task_Part1.cpp
+++ 04_Problem_Solving/04.8_Lab3task_Part1.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-	int main()
-	{
+int main(){
 	char letter;
-	letter=-100;
-	cout<<letter<<"\n";
-	letter=12;
-	cout<<letter<<"\n";
-	cin>>letter;
-		getch();
-	return 0;
 	
+	letter=-100;
+	
+	cout<<letter<<"\n";
+	
+	letter=12;
+	
+	cout<<letter<<"\n";
+	
+	cin>>letter;
+	
+	getch();
+	return 0;
 }
