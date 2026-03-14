a945716e233edcbfa76251a8c2a8d10e8c1f2e8f
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-15 01:30:08 +0500

    Lab 3 Task Part1

---
+++ 04_Problem_Solving/04.8_Lab3task_Part1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+	int main()
+	{
+	char letter;
+	letter=-100;
+	cout<<letter<<"\n";
+	letter=12;
+	cout<<letter<<"\n";
+	cin>>letter;
+		getch();
+	return 0;
+	
+}
