46b221c26b2fae57fbd91326390816877469eda8
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-17 00:19:40 +0500

    Lab 4 Task 1

--- 04_Problem_Solving/04.11_Lab4task1.cpp
+++ 04_Problem_Solving/04.11_Lab4task1.cpp
@@ -1,37 +1,46 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int main()
-{
+int main(){
+	
 	int a;
 	cout<<"Enter the number of day of week"<<endl;
 	cin>>a;
-	switch(a)
-	{
-	case 1:
-	{cout<<"It's Monday.."<<endl;}
-	break;
-	case 2:{cout<<"It's Tuesday.."<<endl;
-	}
+	
+	switch(a){
+		case 1:{
+			cout<<"It's Monday.."<<endl;
+		}
 		break;
-	case 3:{cout<<"It's Wednesday.."<<endl;
-	}
+	
+		case 2:{cout<<"It's Tuesday.."<<endl;
+		}
 		break;
-	case 4:{cout<<"It's Thursday.."<<endl;
-	}
+	
+		case 3:{cout<<"It's Wednesday.."<<endl;
+		}
 		break;
-	case 5:{cout<<"It's Friday.."<<endl;
-	}
+	
+		case 4:{cout<<"It's Thursday.."<<endl;
+		}
 		break;
-	case 6:{cout<<"It's Saturday.."<<endl;
-	}
+	
+		case 5:{cout<<"It's Friday.."<<endl;
+		}
 		break;
-	case 7:{cout<<"It's Sunday.."<<endl;
-	}
+	
+		case 6:{cout<<"It's Saturday.."<<endl;
+		}
 		break;
-	default:
+	
+		case 7:{cout<<"It's Sunday.."<<endl;
+		}
+		break;
+	
+		default:
 		cout<<"ERROR,Please enter a number between 1 & 7.."<<endl;
 	}
-		getch();
+	
+	getch();
 	return 0;
 }
