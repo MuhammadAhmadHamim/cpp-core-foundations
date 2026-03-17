9324f45b9797d7842dc78af57efeb6d3ca9adb8f
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-18 01:37:00 +0500

    Lab 4 Task 2

---
+++ 04_Problem_Solving/04.12_Lab4task2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	
+	int a;
+	cout<<"Enter any Number"<<endl;
+	cin>>a;
+	
+	if(a%2==0){
+		cout<<"It's an Even Number.."<<endl;
+	}
+	
+	else{
+		cout<<"It's an Odd Number.."<<endl;
+	}
+	
+	getch();
+	return 0;
+}
