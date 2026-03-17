4eb600374bf8f2fb3a656a557a6b5522b547d16c
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-18 01:37:14 +0500

    Lab 4 Task 3

---
+++ 04_Problem_Solving/04.13_Lab4task3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	float temperature;
+	int temp;
+	cout<<"Choose your option:"<<endl;
+	cout<<"1.Convert Celsius to Fahrenheit"<<endl;
+	cout<<"2.Convert Celsius to Kelvin"<<endl;
+	cin>>temp;
+	cout<<"Enter the temperature in Celsius:"<<endl;
+	cin>>temperature;
+	switch(temp)
+	{
+	case 1:	
+	cout<<"Temperature in Fahrenheit: "<<(temperature*9/5)+32;
+	cout<<"F"<<endl;
+	break;
+	
+	case 2:
+	cout<<"Temperature in Kelvin: "<<temperature+273.15;
+	cout<<"K"<<endl;
+	break;
+	default:
+	cout<<"ERROR,Please choose any of the two options"<<endl;
+	}
+		getch();
+	return 0;	
+}
