a150d9118f6069acb3782cf7bfd29a4115cdd022
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-03-19 00:24:44 +0500

    Time for some logic polishing through Untitled(s)

--- 04_Problem_Solving/04.14_Untitled1.cpp
+++ 04_Problem_Solving/04.14_Untitled1.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int main()
-{
+int main(){
+	
 	int i;
+	
 	for(i= 65;i<= 650;i+=65){
 		cout<<"65 x "<<(i/65)<<"= "<<i<<endl;
 	} 
-		getch();
+	
+	getch();
 	return 0;
-	
-	
 }
